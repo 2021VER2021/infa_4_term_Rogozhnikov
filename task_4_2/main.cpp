6f8d7409144a10b606e8a7f2f73da3ee1ad902e4
VER2021VER <rogozhnikov.ve@phystech.edu>
2023-03-30 20:24:48 +0300

    Move parallel for_each realization from main to parallel_for_each function. Remove get() after wait()

--- task_4_2/main.cpp
+++ task_4_2/main.cpp
@@ -5,14 +5,29 @@
 #include <iterator>
 #include <numeric>
 
-class Parallel_for{
+class Parallel_for_bodge{
 public:
-    template <typename Iter>
-    void parallel_for_each_block(Iter start, Iter end, void (*f)(int&)){
+    template <typename Iter, typename T>
+    void for_each(Iter start, Iter end, void (*f)(T&)){
     std::for_each(start, end, f);
 }
 };
 
+template <typename Iter, typename T>
+void parallel_for_each(Iter begin, Iter end, void (*f)(int&)){
+    std::vector<std::future<void>> futures;
+    auto num_threads = std::thread::hardware_concurrency();
+    Parallel_for_bodge F;
+    auto delta = std::distance(begin, end) / num_threads;
+    for (auto i = 0u; i+1 <= num_threads; i++){
+        futures.push_back(std::async(std::launch::async,
+         &Parallel_for_bodge::for_each<Iter, T>,
+          &F, begin + delta * i, begin + delta * (i + 1), f));
+    } 
+    for (auto i = 0; i+1 < num_threads; i++){
+        futures[i].wait();
+    }
+}
 
 void d(int &a){
     a+=2;
@@ -22,22 +37,9 @@ void d(int &a){
 int main(){
     std::vector<int> v(10);
     std::iota(std::begin(v), std::end(v), 1);
-    std::vector<std::future<void>> futures;
-    auto num_threads = std::thread::hardware_concurrency();
-    std::cout << num_threads << std::endl;
-    Parallel_for F;
+    parallel_for_each<std::vector<int>::iterator, int>(std::begin(v), std::end(v), d);
 
-    auto delta = std::distance(std::begin(v), std::end(v))/num_threads;
-    for (auto i = 0u; i+1 <= num_threads; i++){
-        futures.push_back(std::async(std::launch::async, &Parallel_for::parallel_for_each_block<std::vector<int>::iterator>, &F, std::begin(v) + delta*i, std::begin(v) + delta*(i+1),d));
-    } 
-    for (auto i = 0; i+1 < num_threads; i++){
-        futures[i].wait();
-    }
-    for (auto i = 0; i+1 < num_threads; i++){
-        futures[i].get();
-    }
-    for (auto i:v){
+    for (auto i : v){
         std::cout << i << ' ';
     }
     std::cout << std::endl;
